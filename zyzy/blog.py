cd8294b8f8af6034c82155368e5b56be0b9ecd97
wrfly <mr.wrfly@gmail.com>
2018-01-30 02:04:30 +0800

    em...

--- zyzy/blog.py
+++ zyzy/blog.py
@@ -3,46 +3,65 @@
 '''
 zyzy blog
 '''
+
 import time
 import os
+import subprocess
+from shutil import copyfile
 
-DIR = "/tmp/posts"
+HUGO_SITE_DIR = "/root/Documents/i.kfd.me"
+POST_DIR = os.path.join(HUGO_SITE_DIR, "content")
+IMG_DIR = os.path.join(HUGO_SITE_DIR, "static/img")
 
 class blog():
     """
     blog("hello").print_post()
     blog("hello",["tag1","你好"],"tttttitle").print_post()
     """
-    def __init__(self, content, tags=["碎碎念"], title=None, filename=None):
+    def __init__(self, content, tags=None, title=None, image=None):
         date = str(int(time.time()))
         self.content = content
-        self.tags = tags
+        self.image = image
+        if tags:
+            self.tags = tags
+        else:
+            self.tags = ["碎碎念"]
         self.date = date
         self.post = ""
         if title:
             self.title = title
         else:
             self.title = "_"
-        if filename:
-            self.filename = filename
-        else:
-            self.filename = date
-        self.filename += ".md"
+        self.filename = date+".md"
     
     def format_post(self):
-        template = """___
+        template = """---
 title: {}
 date: {}
 tags: {}
 ---
 {}"""
+        self.content = self.content
+        self.content = self.content.replace("\n\n", "#N#")
+        self.content = self.content.replace("\n", "<br />")
+        self.content = self.content.replace("#N#", "\n\n")
+        if self.image:
+            new_path = os.path.join(IMG_DIR, self.date+".jpg")
+            copyfile(self.image, new_path)
+            img_path = "/img/"+self.date+".jpg"
+            self.content = "![{}]({})\n\n".format(self.date, img_path) \
+                + self.content
         self.post = template.format(self.title, self.date,
-            str(self.tags).decode('string_escape'),
+            self.tags,
+            # str(self.tags).decode('string_escape'),
             self.content)
 
     def write_post(self):
-        with open(os.path.join(DIR,self.filename),"w") as f:
+        self.format_post()
+        with open(os.path.join(POST_DIR, self.filename),"w") as f:
             f.write(self.post)
+        # hugo -s /root/Documents/i.kfd.me
+        subprocess.call(["hugo", "-s", HUGO_SITE_DIR])
     
     def print_post(self):
         self.format_post()
--- zyzy/bot.py
+++ zyzy/bot.py
@@ -3,12 +3,13 @@
 '''
 zyzy blog bot
 '''
+import logging
+import os
+import blog
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import (Updater, CommandHandler, MessageHandler,
             Filters, RegexHandler, ConversationHandler)
 
-import logging
-import os
 
 TOKEN = os.getenv("TG_BOT_TOKEN")
 
@@ -18,24 +19,48 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-PHOTO, WORDS, TAGS = range(3)
+PHOTO, WORDS, TAGS, WRITE = range(4)
+
+post = dict()
+user_photo = 'user_photo.jpg'
+
+def prepare():
+    post = dict()
+    try:
+        os.remove(user_photo)
+    except Exception as e:
+        pass
+
+def end():
+    pass
+
+def write(update):
+    logger.info("content: %s; tags: %s; photo: %s",
+        post.get("words"), post.get("tags"), post.get("photo"))
+    update.message.reply_text('done')
+    tags = None
+    if post.get("tags"):
+        tags = []
+        for tag in post.get("tags").split(" "):
+            tags.append(tag)
+    b = blog.blog(post.get("words"), tags, image=post.get("photo"))
+    b.write_post()
+    end()
+    return ConversationHandler.END
+
 
 def start(bot, update):
-    # reply_keyboard = [['Boy', 'Girl', 'Other']]
-
-    # update.message.reply_text(
-    #     'Hi! My name is Professor Bot. I will hold a conversation with you. '
-    #     'Send /cancel to stop talking to me.\n\n'
-    #     'Are you a boy or a girl?',
-    #     reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    prepare()
     update.message.reply_text("Send me a photo or /cancel or /skip")
+    post["photo"] = False
     return PHOTO
 
 def photo(bot, update):
     user = update.message.from_user
     photo_file = bot.get_file(update.message.photo[-1].file_id)
-    photo_file.download('user_photo.jpg')
-    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+    photo_file.download(user_photo)
+    post["photo"] = user_photo
+    logger.info("Photo of %s: %s", user.first_name, user_photo)
     update.message.reply_text('Gorgeous! Now, send me some words, '
                               'or send /skip if you don\'t want to.')
 
@@ -54,38 +79,35 @@ def skip_photo(bot, update):
 def words(bot, update):
     user = update.message.from_user
     logger.info("words of %s: %s", user.first_name, update.message.text)
-    update.message.reply_text('And tags:')
-
+    post["words"] = update.message.text
+    update.message.reply_text('tags or /skip')
     return TAGS
 
 def skip_words(bot, update):
     user = update.message.from_user
     logger.info("skip words")
-    update.message.reply_text('Bye')
-
+    update.message.reply_text('tags or /skip')
     return TAGS
 
 def tags(bot, update):
     user = update.message.from_user
     logger.info("Tags of %s: %s", user.first_name, update.message.text)
-    update.message.reply_text('I got it, bye')
-
-    return ConversationHandler.END
+    post["tags"] = update.message.text
+    update.message.reply_text('now write to new post')
+    return write(update)
 
 def skip_tags(bot, update):
     user = update.message.from_user
     logger.info("User %s did not send a tag.", user.first_name)
-    update.message.reply_text('Bye')
-
-    return ConversationHandler.END
-
+    update.message.reply_text('skip tags and write to new post.')
+    return write(update)
 
 def cancel(bot, update):
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
     update.message.reply_text('Bye! I hope we can talk again some day.',
                               reply_markup=ReplyKeyboardRemove())
-
+    end()
     return ConversationHandler.END
 
 
